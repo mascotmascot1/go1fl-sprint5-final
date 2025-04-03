3dece0dba9d7d44e8174a4c111c1ce23fce5ff4f
Roman Sh. <rsheptytsky@gmail.com>
2025-04-03 22:20:36 +0400

    Implement functionality for internal packages

--- internal/actioninfo/actioninfo.go
+++ internal/actioninfo/actioninfo.go
@@ -1,11 +1,41 @@
 package actioninfo
 
-import (
-	...
-)
+import "fmt"
 
-// создайте интерфейс DataParser
-...
+// DataParser содержит методы для парсинга данных и вывода информации об активности.
+type DataParser interface {
+	// Parse обрабатывает строку с данными и заполняет структуру, реализующую интерфейс DataParser.
+	// Формат входных данных зависит от конкретной структуры (например, Training или DaySteps).
+	Parse(datastring string) (err error)
 
-// создайте функцию Info()
-...
+	// ActionInfo формирует и возвращает строку с данными об активности.
+	// Формат выходных данных зависит от конкретной структуры (например, Training или DaySteps).
+	ActionInfo() (string, error)
+}
+
+// Info обрабатывает набор данных о тренировках или прогулках.
+//
+// Параметры:
+//
+// dataset []string — слайс строк с данными об активности.
+// dp DataParser — объект, реализующий интерфейс DataParser (например, Training или DaySteps).
+func Info(dataset []string, dp DataParser) {
+	if dp == nil {
+		fmt.Println("Error: DataParser instance is nil")
+		return
+	}
+
+	for _, data := range dataset {
+		err := dp.Parse(data)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		summary, err := dp.ActionInfo()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(summary)
+	}
+}
--- internal/daysteps/daysteps.go
+++ internal/daysteps/daysteps.go
@@ -1,19 +1,61 @@
 package daysteps
 
 import (
-	...
+	"fmt"
+	"strconv"
+	"strings"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/personaldata"
+	"github.com/Yandex-Practicum/go1fl-sprint5-final/internal/spentenergy"
 )
 
-const (
-	StepLength = 0.65
-)
+// DaySteps содержит информацию о дневных прогулках пользователя.
+type DaySteps struct {
+	personaldata.Personal               // данные пользователя (имя, вес, рост).
+	Steps                 int           // количество шагов.
+	Duration              time.Duration // длительность прогулки.
+}
 
-// создайте структуру DaySteps
-...
+// Parse парсит строку с данными формата "678,0h50m"
+// и записывает данные в соответствующие поля структуры DaySteps.
+//
+// Параметры:
+//
+// datastring string — строка, содержащая количество шагов и продолжительность, разделённые запятой.
+func (ds *DaySteps) Parse(datastring string) (err error) {
+	parts := strings.Split(datastring, ",")
+	if len(parts) != 2 {
+		return fmt.Errorf("invalid data format: expected 2 values, got %d, "+
+			"data: %q", len(parts), datastring)
+	}
 
+	steps, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return err
+	}
+	ds.Steps = steps
 
-// создайте метод Parse()
-...
+	duration, err := time.ParseDuration(parts[1])
+	if err != nil {
+		return err
+	}
+	ds.Duration = duration
+	return nil
+}
 
-// создайте метод ActionInfo()
-...
+// ActionInfo формирует и возвращает строку с данными о тренировке:
+// количество шагов, дистанцию (км.) и количество сожжённых калорий.
+func (ds DaySteps) ActionInfo() (string, error) {
+	if ds.Duration <= 0 {
+		return "", fmt.Errorf("duration must be greater than zero")
+	}
+	distance := spentenergy.Distance(ds.Steps)
+	calories, err := spentenergy.WalkingSpentCalories(ds.Steps, ds.Weight, ds.Height, ds.Duration)
+	if err != nil {
+		return "", err
+	}
+	summary := fmt.Sprintf("Количество шагов: %d.\nДистанция составила %.2f км.\n"+
+		"Вы сожгли %.2f ккал.", ds.Steps, distance, calories)
+	return summary, nil
+}
